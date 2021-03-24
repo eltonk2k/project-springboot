c5ee3e4fffd4246fea4b707f42c9c5d3b8262ea3
elielton silva <elieltonpb1@gmail.com>
2021-03-24 16:02:23 -0300

    Adicionando Entidade Produto

---
+++ src/main/java/com/projeto/curso/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.projeto.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projeto.curso.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+
+}
