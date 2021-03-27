d6ce7ab803e4d9b1eed161ae5b7f693e436dcfcd
elielton silva <elieltonpb1@gmail.com>
2021-03-26 21:29:27 -0300

    Tratamento de exceção

---
+++ src/main/java/com/projeto/curso/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.projeto.curso.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("Resourse not found. Id " + id);
+	}
+	
+
+}
